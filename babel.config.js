64989aafe31a4f6226dfdd73279dc2287cdafd17
崔同洋 <cuitongyang@accesscorporate.com.cn>
2021-04-19 23:03:46 +0800

    feat: 配置绝对路径babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@/assets': './src/assets',
+          '@/components': './src/components',
+          '@/config': './src/config',
+          '@/models': './src/models',
+          '@/navigator': './src/navigator',
+          '@/pages': './src/pages',
+          '@/utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {test} from '@/utils/index';
 
 class App extends React.PureComponent {
+  componentDidMount() {
+    // eslint-disable-next-line jest/no-disabled-tests
+    test();
+  }
   render() {
     return (
       <View>
