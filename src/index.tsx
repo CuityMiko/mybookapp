2cddaf3a8b3790733e6bd1f50eef8ae4cff2a809
崔同洋 <cuitongyang@accesscorporate.com.cn>
2021-04-19 22:43:34 +0800

    目录结构

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <View>
+        <Text>App.tsx</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
